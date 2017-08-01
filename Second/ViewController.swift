a0eb4089564ea868658b5cd753121bd8d59d6711
Malena <malena@malenas-macbook-pro.local>
2017-08-01 10:01:26 +0200

    Initial Commit

---
+++ Second/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Second
+//
+//  Created by Malena on 8/1/17.
+//  Copyright © 2017 CloudHunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
